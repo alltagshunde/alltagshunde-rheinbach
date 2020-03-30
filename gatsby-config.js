62efeac220b37536e1f713ff075489d89fede117
azahnen <zahnen@interactive-instruments.de>
2020-03-30 16:57:50 +0200

    init

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: `Alltagshunde Rheinbach`,
+    description: `Alltagshunde Rheinbach - Carolin Henseler`,
+    author: `Carolin Henseler`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-building-blocks`,
+      options: {
+      },
+    },
+  ],
+}
+
